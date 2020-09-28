47e243b047500fbf1e2cbf89a497b22825a2ca27
mariofrescas <mario.frescas.marquez@gmail.com>
2020-09-28 18:08:14 -0500

    Agrega la base del appbar

--- flutter/custom_appbar/lib/main.dart
+++ flutter/custom_appbar/lib/main.dart
@@ -14,10 +14,28 @@ class App extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  static const double ICON_SIZE = 28;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Content'))
+      body: Center(child: Text('Content')),
+      appBar: AppBar(
+        centerTitle: true,
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        title: Container(
+          height: kToolbarHeight,
+          decoration: BoxDecoration(
+            color: Color(0xFF3F51B5),
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
+          ),
+        ),
+        leading: IconButton(icon: Icon(Icons.menu), color: Color(0xFF3F51B5), iconSize: ICON_SIZE, onPressed: () => { }),
+        actions: [
+          IconButton(icon: Icon(Icons.more_vert), color: Color(0xFF3F51B5), iconSize: ICON_SIZE, onPressed: () => { })
+        ],
+      )
     );
   }
 }
