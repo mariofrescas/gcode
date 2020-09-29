7a1082db266e7a6877150b5317b3bd6943783d8c
mariofrescas <mario.frescas.marquez@gmail.com>
2020-09-29 15:35:11 -0500

    Separa AppBar en su propio archivo

---
+++ flutter/custom_appbar/lib/app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'custom_appbar',
+      home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  static const double ICON_SIZE = 28;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Content')),
+      appBar: AppBar(
+        centerTitle: true,
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        title: Container(
+          height: kToolbarHeight,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 5
+              )
+            ],
+            color: Color(0xFF3F51B5),
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
+          ),
+        ),
+        leading: IconButton(icon: Icon(Icons.menu), color: Color(0xFF3F51B5), iconSize: ICON_SIZE, onPressed: () => { }),
+        actions: [
+          IconButton(icon: Icon(Icons.more_vert), color: Color(0xFF3F51B5), iconSize: ICON_SIZE, onPressed: () => { })
+        ],
+      )
+    );
+  }
+}
--- flutter/custom_appbar/lib/main.dart
+++ flutter/custom_appbar/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'app_bar.dart';
+
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Color(0xFF303E8B),
@@ -11,45 +13,3 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'custom_appbar',
-      home: Home()
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  static const double ICON_SIZE = 28;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Content')),
-      appBar: AppBar(
-        centerTitle: true,
-        shadowColor: Colors.transparent,
-        backgroundColor: Colors.transparent,
-        title: Container(
-          height: kToolbarHeight,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 5
-              )
-            ],
-            color: Color(0xFF3F51B5),
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
-          ),
-        ),
-        leading: IconButton(icon: Icon(Icons.menu), color: Color(0xFF3F51B5), iconSize: ICON_SIZE, onPressed: () => { }),
-        actions: [
-          IconButton(icon: Icon(Icons.more_vert), color: Color(0xFF3F51B5), iconSize: ICON_SIZE, onPressed: () => { })
-        ],
-      )
-    );
-  }
-}
